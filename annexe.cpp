0b3782be0a5e47a734e4e4481a145752a8ef600b
Naludrag <stephanecarvalho81@gmail.com>
2019-01-08 13:57:18 +0100

    test

--- annexe.cpp
+++ annexe.cpp
@@ -1,5 +1,25 @@
-//
-// Created by Arthur on 08.01.2019.
-//
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : <nn>
+ Fichier     : <nom du fichier>.cpp
+ Auteur(s)   : <prénom> <nom>
+ Date        : <jj.mm.aaaa>
 
+ But         : <à compléter>
+
+ Remarque(s) : <à compléter>
+
+ Compilateur : MinGW-g++ <x.y.z>
+ -----------------------------------------------------------------------------------
+ */
 #include "annexe.h"
+
+using namespace std;
+
+string coder(const string& message, const vector<int>& cle)
+{
+   for(size_t i = 0; i < message.size();i++)
+   {
+       
+   }
+}
